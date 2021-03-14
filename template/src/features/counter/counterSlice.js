508173a6aa06e1006355a581066a7e6c28c90cbc
Rahsheen Porter <rahsheen.porter@gmail.com>
2021-03-14 15:24:38 -0400

    Move deps closer to default template

--- template/src/features/counter/counterSlice.js
+++ template/src/features/counter/counterSlice.js
@@ -1,11 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   value: 0,
 };
 
 export const counterSlice = createSlice({
-  name: "counter",
+  name: 'counter',
   initialState,
   reducers: {
     increment: (state) => {
@@ -18,13 +18,14 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const {increment, decrement, incrementByAmount} = counterSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
@@ -38,7 +39,7 @@ export const incrementAsync = (amount) => (dispatch) => {
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
